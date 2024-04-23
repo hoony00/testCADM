7e22044bbea16061e5631beb5169655e1d337136
Sanghoon Lee <nasi3611@naver.com>
2024-04-23 15:52:56 +0900

    feat : 이미지 업로드 후 상품받기

--- lib/provider/camera/send_pic.dart
+++ lib/provider/camera/send_pic.dart
@@ -7,12 +7,12 @@ import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 
 import '../../common/exception/exception.dart';
-import '../../view/screen/add/s_check.dart';
 
 class SelectedImgNotifier extends StateNotifier<List<XFile>> {
   SelectedImgNotifier() : super([]);
 
   final String url = 'https://88c9-61-98-7-200.ngrok-free.app';
+  final ExceptionDio exceptionDio = ExceptionDio();
 
   // 이미지 추가
   void insertImg(List<XFile> list) {
@@ -60,6 +60,7 @@ class SelectedImgNotifier extends StateNotifier<List<XFile>> {
     debugPrint("========== 사진 업로드 시작 ==========");
 
     try {
+      List<String> imageURLs = [];
       for (var i = 0; i < state.length; i++) {
         var imageType = state[i].path.split('.').last;
         //확장자 구별
@@ -72,11 +73,20 @@ class SelectedImgNotifier extends StateNotifier<List<XFile>> {
 
         // 이미지 업로드 요청
         var response = await uploadImage(compressedFile, i, imageType);
-        await handleResponse(response, "이미지 저장");
+        await handleResponse(response, "이미지 저장", isDioResponse: response is Response);
 
         if (response.statusCode == 200) {
           debugPrint("<--이미지 업로드 성공 OK! -->");
+
+          // 응답에서 이미지 URL 추출
+          var imageURL = response.body;
+          imageURLs.add(imageURL);
         }
+      }
+
+      if(imageURLs.isNotEmpty){
+        // 상품 등록
+        await registerProduct(imageURLs);
       }
     } catch (e) {
       if (e is DioException) {
@@ -97,7 +107,7 @@ class SelectedImgNotifier extends StateNotifier<List<XFile>> {
       'images',
       stream,
       length,
-      filename: 'saleIdx_$index.jpg', // 확장자를 .jpg로 변경
+      filename: 'saleIdx_$index.$imageType', // 확장자를 .jpg로 변경
     );
     request.files.add(multipartFile);
     var response = await http.Response.fromStream(await request.send());
@@ -114,15 +124,53 @@ class SelectedImgNotifier extends StateNotifier<List<XFile>> {
     );
   }
 
+  // 이미지 업로드 후 상품 등록
+  Future<void> registerProduct(List<String> imageURLs) async {
+    try {
+      var dio = Dio();
+
+      // 상품 등록 요청
+      Response saleResponse = await dio.post(
+        '$url/sales',
+        data: {
+          "price": 10000000, // 가격은 더미 데이터로 설정
+          "mark": true, // 마크는 더미 데이터로 설정
+          "option": "레이저 장치", // 옵션은 더미 데이터로 설정
+          "product": {
+            "name": "더미 상품", // 상품명은 더미 데이터로 설정
+            "type": "레이저", // 타입은 더미 데이터로 설정
+            "year": 2024, // 년도는 더미 데이터로 설정
+            "company": "더미 회사", // 회사는 더미 데이터로 설정
+            "area": "서울" // 지역은 더미 데이터로 설정
+          },
+          "images": imageURLs,
+        },
+      );
+
+      await handleResponse(saleResponse, "상품 등록", isDioResponse: true);
+    } catch (e) {
+      if (e is DioException) {
+        await handleDioError(e);
+      } else {
+        debugPrint("알 수 없는 오류: $e");
+      }
+    }
+  }
+
   // HTTP 응답 처리
-  Future<void> handleResponse(http.Response response, String action) async {
-    await ExceptionDio().handleHttpResponseCode(response.statusCode, action);
+  Future<void> handleResponse(dynamic response, String action, {bool isDioResponse = false}) async {
+    if (isDioResponse) {
+      await exceptionDio.handleHttpResponseCode(response.statusCode!, action);
+    } else {
+      await exceptionDio.handleHttpResponseCode(response.statusCode, action);
+    }
   }
 
   // Dio 오류 처리
   Future<void> handleDioError(DioException e) async {
-    await ExceptionDio().handleDioError(e);
+    await exceptionDio.handleDioError(e);
   }
+
 }
 
 final selectedImgProvider = StateNotifierProvider<SelectedImgNotifier, List<XFile>>((ref) {
